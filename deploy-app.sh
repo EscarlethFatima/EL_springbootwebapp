3b2e7fabd18dc3722cec40fbe56c98d77988a769
EscarlethFatima <Escarleth.Ledezma@fundacion-jala.org>
2018-08-16 19:10:55 +0000

    changes3

--- deploy-app.sh
+++ deploy-app.sh
@@ -4,4 +4,4 @@ sudo systemctl stop tomcat
 rm -rf /home/ubuntu/springbootwebapp_deploy
 mkdir /home/ubuntu/springbootwebapp_deploy
 sudo cp -f /tmp/spring-boot-web-0.0.1-SNAPSHOT-capsule.jar /home/ubuntu/springbootwebapp_deploy/spring-boot-web-0.0.1-SNAPSHOT-capsule.jar
-nohup java -jar /home/ubuntu/springbootwebapp_deploy/spring-boot-web-0.0.1-SNAPSHOT-capsule.jar &
+sudo nohup java -jar /home/ubuntu/springbootwebapp_deploy/spring-boot-web-0.0.1-SNAPSHOT-capsule.jar &
